4cb11e078ef5fb3af7a1e65df0f411773eac0d07
fahad <fahadmalik11101@gmail.com>
2019-05-02 16:52:03 +0500

    dasdads
    asdasdasd

---
+++ loop/loop/Looping.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop
+
+{
+
+    class Looping
+    {
+        public static void drawlineUsingWhileLoop()    
+        {
+            int i = 0;
+            while (i<8)
+        }
+            Console.WriteLine("\t\t||||");
+            int++;  
+    }
+}
+        public static void for loop(int e, int s)
+    }
+        for (int i = s; i < e; i++)
+	{
+	
+        for (int sp = e; sp >= i; sp--)
+            Console.Write("   ");			
+
+        for (int j = s; j < 2*i-1; j++)
+            Console.Write(" * ");
+			
+        Console.WriteLine();
+
+
+{
+			 
+			}
+
+{
+			 
+			}
+        
+        
+
+    }
+}
